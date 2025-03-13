d27c0d87fa07a6b62cf0847f3d8c29fe1903cb0e
indura08 <induraperera3@gmail.com>
2025-03-13 17:44:43 +0530

    adding dependancy installation and data model creation

---
+++ UserService/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "An email is required")]
+        public string EmailAddress { get; set; } = null!;
+        [Required(ErrorMessage = "A password is must")]
+        public string Password { get; set; } = null!;
+        [Required(ErrorMessage = "Without a name how can we identify you ? with primary key value?")]
+        public string Name { get; set; } = null!; 
+    }
+}
