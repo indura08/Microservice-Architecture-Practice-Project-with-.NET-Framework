321ef362257ccfdf0c3abe58289623d6fd98b287
indura08 <induraperera3@gmail.com>
2025-03-30 13:13:12 +0530

    fixing issue #6 - mplmeneted all the codes for autherization and authentication part

--- UserService/Repository/AccountRepository.cs
+++ UserService/Repository/AccountRepository.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using UserService.Data.Services;
 using UserService.Model;
 using UserService.Model.DTOs;
@@ -24,13 +28,59 @@ namespace UserService.Repository
             if (user is not null) return new ServiceResponse.GeneralResponse(false, "User already registered");
 
             var createUser = await userManager.CreateAsync(newUser!, userDto.Password);
+            if (!createUser.Succeeded) return new ServiceResponse.GeneralResponse(false, "An error occured , please try again");
 
-            return null;
+            var checkUser = await roleManager.FindByNameAsync("USER");
+            if (checkUser is null) await roleManager.CreateAsync(new IdentityRole() { Name = "USER" });
+
+            await userManager.AddToRoleAsync(newUser, "USER");
+            return new ServiceResponse.GeneralResponse(true, "Acccount created successfully");
         }
 
-        public Task<ServiceResponse.LoginResponse> Login(LoginDTO loginDto)
+        public async Task<ServiceResponse.LoginResponse> Login(LoginDTO loginDto)
         {
-            throw new NotImplementedException();
+            if (loginDto is null) return new ServiceResponse.LoginResponse(false, null!, "Login Credentials are not complete , please check again");
+
+            var getUser = await userManager.FindByEmailAsync(loginDto.EmailAddress);
+            if (getUser is null) return new ServiceResponse.LoginResponse(false, null!, "User Not found , try again");
+
+            bool checkPassword = await userManager.CheckPasswordAsync(getUser, loginDto.Password);
+
+            if (!checkPassword)
+            {
+                return new ServiceResponse.LoginResponse(false, null!, "Invalid email/password");
+            }
+
+            var getUserRole = await userManager.GetRolesAsync(getUser);
+            var userSession = new UserSession(getUser.Id, getUser.Name, getUser.Email, getUserRole.First());
+
+            string token = GenerateToken(userSession);
+            return new ServiceResponse.LoginResponse(true, token!, "Login successfull");
+        }
+
+        private string GenerateToken(UserSession user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id!),
+                new Claim(ClaimTypes.Name, user.Name!),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Role, user.Role!)
+            };
+
+            var Token = new JwtSecurityToken
+            (
+                issuer: config["Jwt:Issuer"],
+                audience: config["Jwt:Audience"],
+                claims: userClaims,
+                expires: DateTime.Now.AddDays(2),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(Token);
         }
     }
 }
